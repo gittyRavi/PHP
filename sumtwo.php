cae14cdeacb7bc2d690e790a0415d44c3411c3b8
gittyRavi <rairavisaxena@gmail.com>
2016-12-03 16:15:27 +0530

    Create sumtwo.php

---
+++ sumtwo.php
@@ -0,0 +1,16 @@
+<html>
+<head>First PHP Page</head>
+<title class=t>PHP-pg.1</title>
+<Style type="text/CSS">
+	.t{color : Blue;}
+</Style>
+<body>
+<?php
+echo '<br><a href="file:///C:/Users/ravi/Documents/notepad++/new%202.html"><h4 class=t>Hello </h4></a><br>';
+$x=5;
+$y=5;
+$z=$x+$y;
+echo "sum of x(=5) and y(=5) is ".'<a href="http://localhost/Files/new%202.php"><abbr title="x+y" class=t>'.$z.'</abbr></a>'."<br>";
+?>
+</body>
+</html>
